56145ab62bdd8d37c1cdd491830a056aa04cb0fa
Soojin Lee <46768135+benecia2sj@users.noreply.github.com>
2023-12-10 11:32:33 -0800

    Version 3

--- myfunc.py
+++ myfunc.py
@@ -2,17 +2,17 @@ import os
 import sys
 import numpy as np
 
-# Version 2
-def add(a:int, b:int) -> int:
+# Version 3
+def add(a:float, b:float) -> float:
     """
-    This function adds two integers.
+    This function adds two numbers.
     """
     out = a + b
     return out
 
 def subtract(a:float, b:float) -> float:
     """
-    This function subjects two numbers
+    This function subjects two numbers.
     """
     out = a - b
     return float(out)
